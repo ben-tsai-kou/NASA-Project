dad2e2a9d1671309788f9f709e4a0fbe6db03377
benwhite0820 <dragon7230@gmail.com>
2023-12-25 23:31:26 +0800

    cors middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 
 // routers
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
 app.use(express.json());
 app.use(planetsRouter);
 
