509b72c8ce49143675214ba5f110c5e2176608e8
benwhite0820 <dragon7230@gmail.com>
2024-03-30 11:41:10 +0800

    serving react.js front end in production

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -12,6 +13,10 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
+app.get('/', (_, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
