d64404ba2c529bfa339aaf5d73f8d0ad5dc48dba
benwhite0820 <dragon7230@gmail.com>
2024-03-30 12:46:13 +0800

    logging requests with morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,28 @@
 const path = require('path');
+// const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 // routers
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
+// const accessLogStream = fs.createWriteStream(
+//   path.join(__dirname, 'access.log'),
+//   { flags: 'a' }
+// );
+
 app.use(
   cors({
     origin: 'http://localhost:3000',
   })
 );
+
+// app.use(morgan('combined', { stream: accessLogStream }));
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
