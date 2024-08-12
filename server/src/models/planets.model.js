0ffe8a9c4817d4650fc8699d78bb9077f155d1c0
ben-tsai-kou <dragon7230@gmail.com>
2024-08-12 21:19:33 +0900

    creating and inserting documents

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const { parse } = require('csv-parse');
 
+const planets = require('./planets.mongo');
+
 const habitablePlanets = [];
 
 const isHabitablePlanet = (planet) =>
@@ -22,9 +24,10 @@ const loadPlanetsData = () => {
           columns: true,
         })
       )
-      .on('data', (data) => {
+      .on('data', async (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanets.push(data);
+          // TODO: Replace below create with insert + update = upsert
+          //   await planets.create({ keplerNames: data.kepler_name });
         }
       })
       .on('error', (err) => {
