f422d3773229f745ce13d6edc52d3416d7e63c6a
ben-tsai-kou <dragon7230@gmail.com>
2024-08-15 21:45:08 +0900

    referential integrity

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,5 @@
 const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
 
 const launches = new Map();
 
@@ -18,6 +19,14 @@ const launch = {
 launches.set(launch.flightNumber, launch);
 
 const saveLaunch = async (launch) => {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error('No matching planet was found');
+  }
+
   await launchesDatabase.updateOne(
     {
       flightNumber: launch.flightNumber,
