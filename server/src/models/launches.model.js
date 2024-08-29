e1aa1fcf2721f38b02d43ac9ed8a7aef6c622f1e
ben-tsai-kou <dragon7230@gmail.com>
2024-08-29 21:36:55 +0900

    mapping spaceX data to our database

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -103,6 +103,28 @@ const loadLaunchData = async () => {
       ],
     },
   });
+
+  const launchDocs = response.data.docs;
+
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc['payloads'];
+
+    const customers = payloads.flatMap((payload) => {
+      return payload['customers'];
+    });
+
+    const launch = {
+      flightNumber: launchDoc['flight_number'],
+      mission: launchDoc['name'],
+      rocket: launchDoc['rocket']['name'],
+      launchDate: launchDoc['date_local'],
+      upcoming: launchDoc['upcoming'],
+      success: launchDoc['success'],
+      customers,
+    };
+
+    console.log(`${launch.flightNumber} ${launch.mission}`);
+  }
 };
 
 module.exports = {
